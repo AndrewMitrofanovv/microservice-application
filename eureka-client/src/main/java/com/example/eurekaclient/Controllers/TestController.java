c5bc64bf49abe22ca7c87775ad1184712009d0d7
AndrewMitrofanovv <Andrew.Mitrofanovv@yandex.ru>
2023-07-11 16:27:15 +0500

    add eureka-client module

--- eureka-client/src/main/java/com/example/eurekaclient/Controllers/TestController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/Controllers/TestController.java
@@ -1,15 +1,18 @@
 package com.example.eurekaclient.Controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/main")
+@RestController
+@RequestMapping("/main")
 public class TestController {
-
-
+    @Value("${eureka.instance.instance-id}")
+    private String uuid;
 
     @GetMapping("/test")
     public String test(){
-        return "hello world";
+        return uuid;
     }
 }
